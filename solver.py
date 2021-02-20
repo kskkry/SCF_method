06f31c0f51f843064cdc24f8a86e6cb707cf1f4c
KryKsk <koskar0358@gmail.com>
2021-02-20 13:01:11 +0900

    修正

--- solver.py
+++ solver.py
@@ -20,14 +20,13 @@ class SCFSolver:
         self.num_elec = num_elec
         self.num_AO = num_AO
         assert self.Hcore.shape == self.S.shape
-        assert self.Hcore.shape == self.two_elec_integral.shape
 
     def compute_transformation_matrix(self, mode='canonical'):
         '''
         parameter mode should be 'canonical' or 'symmetric'
         return: regular matrix
         '''
-        X = np.array(self.S.shape)
+        X = np.zeros(self.S.shape)
         
         #固有値の配列、固有ベクトルをそれぞれ返す
         e, e_v = np.linalg.eigh(self.S)
@@ -57,15 +56,15 @@ class SCFSolver:
             for j in range(self.num_AO):
                 tmp = 0
                 for k in range(num_occupied_orbital):
-                    tmp += C[i,k]*C[j,k]
-                new_P = tmp
+                    tmp += 2*(C[i,k]*C[j,k])
+                new_P[i,j] = tmp
         return new_P
 
     def compute_term_2_elec_integral(self, P: np.array):
         '''
         2電子積分(self.two_elec_integral)がエルミート行列であることを確認
         '''
-        new_G = np.zeros((2,2))
+        new_G = np.zeros((self.num_AO,self.num_AO))
 
         for i in range(self.num_AO):
             for j in range(self.num_AO):
@@ -73,7 +72,7 @@ class SCFSolver:
                     for l in range(self.num_AO):
                         J = self.two_elec_integral[i][j][k][l]
                         K = 0.5 * self.two_elec_integral[i][l][k][j]
-                        new_G[i,j] += P[k,l] * (J-K)
+                        new_G[i,j] += P[k,l]*(J-K)
         return new_G
 
     def judge_scf_end(self, new_P: np.array, P: np.array):
@@ -92,7 +91,7 @@ class SCFSolver:
         X = self.compute_transformation_matrix()
         X_adj = np.matrix.getH(X) #共役転置行列を得る
         P = np.zeros(self.S.shape)
-        MAX_ITER = 100
+        MAX_ITER = 10
 
         for n_iter in range(MAX_ITER):
             print('-'*50)
@@ -100,15 +99,19 @@ class SCFSolver:
             G = self.compute_term_2_elec_integral(P)
             Fock = self.Hcore + G
             Fock_dash = X_adj.dot(Fock.dot(X))
-            C_dash, eigen_val = np.linalg.eigh(Fock_dash)
+            eigen_val, C_dash = np.linalg.eigh(Fock_dash)
             C = X.dot(C_dash)
             new_P = self.update_P_matrix(P, C)
+
+            #参考: https://qiita.com/hatori_hoku/items/867fa793488ebe1d2beb
+            energy = (new_P * (self.Hcore + Fock)).sum() * 0.5 
+            print(f'Electronic Energy = {energy} hartrees')
             do_finish = self.judge_scf_end(new_P, P)
             if do_finish:
-                print('P')
+                print('Finish SCF Calculation')
+                print('\nComputed Density Matrix')
                 print(new_P)
                 print('-'*50)
                 break
             P = new_P
             print('-'*50)
-
